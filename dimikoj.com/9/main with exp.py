fc640ac6368b508ea150ae2de10409ab8bc1cd67
Ratul Hasan <34002411+RaSan147@users.noreply.github.com>
2023-11-11 22:38:52 +0600

    solve dimik -> 9

--- dimikoj.com/9/main with exp.py
+++ dimikoj.com/9/main with exp.py
@@ -5,4 +5,5 @@ from math import sqrt
 for T in range(int(input())):
 	sq = sqrt(int(input()))
 
+	# now we just gotta check if the number is a float or integer
 	print("NO" if int(sq)!=sq else "YES")  
